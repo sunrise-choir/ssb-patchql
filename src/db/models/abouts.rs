2cd266cf004c5446602d14372688a9fc927e042b
Piet Geursen <pietgeursen@gmail.com>
2019-03-19 09:29:46 +1300

    convert abouts to diesel

--- src/db/models/abouts.rs
+++ src/db/models/abouts.rs
@@ -1,30 +1,35 @@
 use crate::db::{Error, SqliteConnection};
 use crate::lib::*;
+use super::keys::find_or_create_key;
+use super::authors::find_or_create_author;
 use serde_json::Value;
+use diesel::prelude::*;
+use diesel::insert_into;
+use crate::db::schema::abouts::dsl::{abouts, link_from_key_id, link_to_author_id, link_to_key_id};
 
 pub fn insert_abouts(connection: &SqliteConnection, message: &SsbMessage, message_key_id: i32) {
-    unimplemented!();
-    //    if let Value::String(about_key) = &message.value.content["about"] {
-    //        let mut key;
-    //
-    //        let (link_to_author_id, link_to_key_id): (&ToSql, &ToSql) = match about_key.get(0..1) {
-    //            Some("@") => {
-    //                key = find_or_create_author(connection, about_key).unwrap();
-    //                (&key, &Null)
-    //            }
-    //            Some("%") => {
-    //                key = find_or_create_key(connection, about_key).unwrap();
-    //                (&Null, &key)
-    //            }
-    //            _ => (&Null, &Null),
-    //        };
-    //
-    //        let mut insert_abouts_stmt = connection
-    //            .prepare_cached("INSERT INTO abouts_raw (link_from_key_id, link_to_author_id, link_to_key_id) VALUES (?, ?, ?)")
-    //            .unwrap();
-    //
-    //        insert_abouts_stmt
-    //            .execute(&[&message_key_id, link_to_author_id, link_to_key_id])
-    //            .unwrap();
-    //    }
+    if let Value::String(about_key) = &message.value.content["about"] {
+
+        let (link_to_author, link_to_key): (Option<i32>, Option<i32>) = match about_key.get(0..1) {
+            Some("@") => {
+                let key = find_or_create_author(connection, about_key).unwrap();
+                (Some(key), None)
+            }
+            Some("%") => {
+                let key = find_or_create_key(connection, about_key).unwrap();
+                (None, Some(key))
+            }
+            _ => (None, None),
+        };
+
+        insert_into(abouts)
+            .values((
+                    link_from_key_id.eq(message_key_id),
+                    link_to_key_id.eq(link_to_key),
+                    link_to_author_id.eq(link_to_author)
+                    ))
+            .execute(connection)
+            .unwrap();
+
+    }
 }
