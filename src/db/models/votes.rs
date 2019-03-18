4ef5908268e7ea7d7a8037d580151ea7c7d09bc7
Piet Geursen <pietgeursen@gmail.com>
2019-03-19 10:28:06 +1300

    Port votes to diesel.

--- src/db/models/votes.rs
+++ src/db/models/votes.rs
@@ -1,26 +1,29 @@
-use crate::db::{Error, SqliteConnection};
+use crate::db::{SqliteConnection};
 use crate::lib::*;
+use diesel::prelude::*;
+use diesel::replace_into;
+use crate::db::schema::votes::dsl::{votes, link_from_author_id, link_to_key_id, value};
+use serde_json::Value;
+use super::keys::find_or_create_key;
+use super::authors::find_or_create_author;
 
 pub fn insert_or_update_votes(connection: &SqliteConnection, message: &SsbMessage) {
-    unimplemented!()
-    //    if let Value::Number(value) = &message.value.content["vote"]["value"] {
-    //        if let Value::String(link) = &message.value.content["vote"]["link"] {
-    //            let author_id = find_or_create_author(&connection, &message.value.author).unwrap();
-    //            let link_to_key_id = find_or_create_key(connection, link).unwrap();
-    //
-    //            if value.as_i64().unwrap() == 1 {
-    //                connection
-    //                    .prepare_cached("INSERT INTO votes_raw (link_from_author_id, link_to_key_id) VALUES (?, ?)")
-    //                    .unwrap()
-    //                    .execute(&[&author_id, &link_to_key_id])
-    //                    .unwrap();
-    //            } else {
-    //                connection
-    //                    .prepare_cached("DELETE FROM votes_raw WHERE link_from_author_id = ? AND link_to_key_id = ?")
-    //                    .unwrap()
-    //                    .execute(&[&author_id, &link_to_key_id])
-    //                    .unwrap();
-    //            }
-    //        }
-    //    }
+
+    if let Value::Number(vote_value) = &message.value.content["vote"]["value"] {
+        if let Value::String(link) = &message.value.content["vote"]["link"] {
+            let author_id = find_or_create_author(&connection, &message.value.author).unwrap();
+            let link_to_key = find_or_create_key(connection, link).unwrap();
+
+            let vote_num: Option<i32> = vote_value.as_i64().map(|num| num as i32);
+
+            replace_into(votes)
+                .values((
+                        link_from_author_id.eq(author_id),
+                        link_to_key_id.eq(link_to_key),
+                        value.eq(vote_num)
+                        ))
+                .execute(connection)
+                .unwrap();
+        }
+    }
 }
