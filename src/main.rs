5c31fa905d882361767155e201b5f1110cb8365d
Piet Geursen <pietgeursen@gmail.com>
2019-04-06 21:55:42 +1300

    Change to use iron so that cors works.

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,19 @@ extern crate env_logger;
 extern crate juniper_codegen;
 #[macro_use]
 extern crate juniper;
-extern crate juniper_warp;
+extern crate juniper_iron;
 #[macro_use]
 extern crate log as irrelevant_log;
-extern crate warp;
+extern crate iron;
+extern crate logger;
 #[macro_use]
 extern crate diesel_migrations;
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
 extern crate serde_json;
+extern crate mount;
+extern crate iron_cors;
 
 mod db;
 mod graphql;
@@ -23,12 +26,16 @@ mod lib;
 use dotenv::dotenv;
 use flumedb::offset_log::OffsetLog;
 use std::env;
-use warp::{http::Response, log, Filter};
 
 use db::*;
 use graphql::db::DbMutation;
 use graphql::root::*;
 use std::sync::{Arc, Mutex};
+use iron::prelude::*;
+use iron_cors::CorsMiddleware;
+use juniper_iron::{GraphQLHandler, GraphiQLHandler};
+use logger::Logger;
+use mount::Mount;
 
 fn main() {
     env_logger::init();
@@ -37,36 +44,36 @@ fn main() {
     let offset_log_path =
         env::var("OFFSET_LOG_PATH").expect("OFFSET_LOG_PATH environment variable must be set");
 
-    let log = log("ssb-patchql-server");
-
-    let homepage = warp::path::end().map(|| {
-        Response::builder()
-            .header("content-type", "text/html")
-            .body(format!(
-                "<html><h1>juniper_warp</h1><div>visit <a href=\"/graphiql\">/graphiql</a></html>"
-            ))
-    });
-
     let connection = open_connection();
     let locked_connection_ref = Arc::new(Mutex::new(connection));
     let offset_log = OffsetLog::new(offset_log_path).unwrap();
     let locked_log_ref = Arc::new(Mutex::new(offset_log));
 
-    let state = warp::any().map(move || Context {
-        connection: locked_connection_ref.clone(),
-        log: locked_log_ref.clone(),
-    });
-    let graphql_filter =
-        juniper_warp::make_graphql_filter(Schema::new(Query, DbMutation::default()), state.boxed());
+    let mut mount = Mount::new();
 
-    info!("Listening on 127.0.0.1:8080");
-    warp::serve(
-        warp::get2()
-            .and(warp::path("graphiql"))
-            .and(juniper_warp::graphiql_filter("/graphql"))
-            .or(homepage)
-            .or(warp::path("graphql").and(graphql_filter))
-            .with(log),
-    )
-    .run(([127, 0, 0, 1], 8080));
+    let middleware = CorsMiddleware::with_allow_any();
+
+    let graphql_endpoint = GraphQLHandler::new(
+        move |_| Ok(Context{
+            connection: locked_connection_ref.clone(),
+            log: locked_log_ref.clone(),
+        }),
+        Query,
+        DbMutation::default()
+    );
+    let graphiql_endpoint = GraphiQLHandler::new("/graphql");
+
+    mount.mount("/", graphiql_endpoint);
+    mount.mount("/graphql", graphql_endpoint);
+
+    let (logger_before, logger_after) = Logger::new(None);
+
+    let mut chain = Chain::new(mount);
+    chain.link_before(logger_before);
+    chain.link_after(logger_after);
+    chain.link_around(middleware);
+
+    let host = env::var("LISTEN").unwrap_or_else(|_| "0.0.0.0:8080".to_owned());
+    println!("GraphQL server started on {}", host);
+    Iron::new(chain).http(host.as_str()).unwrap();
 }
