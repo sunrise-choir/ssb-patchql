35a734617fc3d9e13ab8af64b1d66e5c251d07a7
Piet Geursen <pietgeursen@gmail.com>
2019-07-03 13:13:23 +1200

    wippy

--- src/bin/json_rpc_stdio.rs
+++ src/bin/json_rpc_stdio.rs
@@ -19,23 +19,36 @@ fn main() {
     dotenv().ok();
     let mut io = IoHandler::default();
 
-    io.add_method("say_hello", |_params| {
-        let offset_log_path =
-            env::var("OFFSET_LOG_PATH").expect("OFFSET_LOG_PATH environment variable must be set");
+    let offset_log_path =
+        env::var("OFFSET_LOG_PATH").expect("OFFSET_LOG_PATH environment variable must be set");
 
-        let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
 
-        let pub_key_string =
-            env::var("SSB_PUB_KEY").expect("SSB_PUB_KEY environment variable must be set");
+    let pub_key_string =
+        env::var("SSB_PUB_KEY").expect("SSB_PUB_KEY environment variable must be set");
 
-        let secret_key_string =
-            env::var("SSB_SECRET_KEY").expect("SSB_SECRET_KEY environment variable must be set");
+    let secret_key_string =
+        env::var("SSB_SECRET_KEY").expect("SSB_SECRET_KEY environment variable must be set");
 
-        let context = Context::new(offset_log_path, database_url, pub_key_string, secret_key_string);
+    let context = Context::new(offset_log_path.clone(), database_url.clone(), pub_key_string.clone(), secret_key_string.clone());
+    let context2 = context.clone();
 
-        let request = GraphQLRequest::new("{dbCursor}".to_string(), None, None);
+    io.add_method("query", move |params: Params| {
+        let request: GraphQLRequest = params.parse()?;
+        
         let root_node = RootNode::new(Query, DbMutation::default() ); 
         let response = request.execute(&root_node, &context);
+
+        let val: Value = serde_json::to_value(response).unwrap();
+        Ok(val)
+    });
+
+
+    io.add_method("process", move |params: Params| {
+        let request: GraphQLRequest = params.parse()?;
+        
+        let root_node = RootNode::new(Query, DbMutation::default() ); 
+        let response = request.execute(&root_node, &context2);
 
         let val: Value = serde_json::to_value(response).unwrap();
         Ok(val)
